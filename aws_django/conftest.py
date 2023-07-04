ffbfd669f4598de6a75aebcbcaa9f6321eb5c3ee
Gentil Ályson Jácome Fernandes <gentil.alyson@nees.ufal.br>
2023-07-04 15:26:08 -0300

    cookie cutter setup

---
+++ aws_django/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from aws_django.users.models import User
+from aws_django.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
